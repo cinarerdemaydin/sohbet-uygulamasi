124b881572728d57e2d1717e29eb2bf458200cb3
cinarerdemaydin <cinarerdemaydin@gmail.com>
2026-09-04 02:40:02 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,47 +1,70 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
+const ROOM_PASSWORD = process.env.ROOM_PASSWORD || "123456";
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
-const ODA_SIFRESI = "1234";
-let activeUsers = {};
+// Kullanıcı verileri: { socketId: { username, color } }
+const users = {};
 
 io.on('connection', (socket) => {
+    
+    socket.on('joinRoom', ({ username, password, color }) => {
+        if (password === ROOM_PASSWORD) {
+            users[socket.id] = { 
+                username: username, 
+                color: color || '#10b981' 
+            };
 
-    socket.on('joinRoom', ({ username, password }) => {
-        if (password === ODA_SIFRESI) {
-            socket.username = username;
-            activeUsers[socket.id] = username;
             socket.emit('loginSuccess');
-            io.emit('updateUserList', Object.values(activeUsers));
+            io.emit('updateUserList', Object.values(users));
+
+            const systemMsg = {
+                user: "Sistem",
+                text: `${username} odaya katıldı.`,
+                time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+                color: "#888888"
+            };
+            io.emit('message', systemMsg);
         } else {
-            socket.emit('loginError', 'Hatalı oda şifresi!');
+            socket.emit('loginError', 'Hatalı Şifre!');
         }
     });
 
     socket.on('chatMessage', (data) => {
-        io.emit('message', {
-            user: data.user,
-            text: data.text,
-            time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-        });
+        const userInfo = users[socket.id];
+        if (userInfo) {
+            io.emit('message', {
+                user: userInfo.username,
+                text: data.text,
+                time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+                color: userInfo.color
+            });
+        }
     });
 
+    // Sesli Sohbet Sinyalleri
     socket.on('joinVoice', () => {
         socket.broadcast.emit('userJoinedVoice', socket.id);
     });
 
     socket.on('signal', (data) => {
-        io.to(data.to).emit('signal', { from: socket.id, signal: data.signal });
+        io.to(data.to).emit('signal', {
+            from: socket.id,
+            signal: data.signal
+        });
+    });
+
+    // Konuşma Durumu (Işık Efekti İçin)
+    socket.on('speakingStatus', (isSpeaking) => {
+        io.emit('userSpeaking', { id: socket.id, isSpeaking });
     });
 
     socket.on('leaveVoice', () => {
@@ -49,9 +72,17 @@ io.on('connection', (socket) => {
     });
 
     socket.on('disconnect', () => {
-        if (socket.id in activeUsers) {
-            delete activeUsers[socket.id];
-            io.emit('updateUserList', Object.values(activeUsers));
+        const userInfo = users[socket.id];
+        if (userInfo) {
+            const systemMsg = {
+                user: "Sistem",
+                text: `${userInfo.username} ayrıldı.`,
+                time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }),
+                color: "#888888"
+            };
+            delete users[socket.id];
+            io.emit('updateUserList', Object.values(users));
+            io.emit('message', systemMsg);
             socket.broadcast.emit('userLeftVoice', socket.id);
         }
     });
@@ -59,5 +90,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Sunucu aktif: http://localhost:${PORT}`);
+    console.log(`Sunucu ${PORT} portunda çalışıyor...`);
 });
